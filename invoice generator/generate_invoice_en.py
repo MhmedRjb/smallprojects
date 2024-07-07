279b84249d8943485ec1e71acfddce7389264923
unknown <72052305+MhmedRjb@users.noreply.github.com>
2024-07-07 11:48:57 +0300

    main

--- invoice generator/generate_invoice_en.py
+++ invoice generator/generate_invoice_en.py
@@ -2,29 +2,29 @@ from jinja2 import Template
 
 # Context data
 context = {
-    'brand_name': 'شركة الـــفــــولاني',
-    'company_name': 'شركة العــــلاني',
-    'address': '24 شارع النصر تفرع مصطفي صدقي -مدينة الهدى -القاهرة',
+    'company': 'شركة الـــفــــولاني',
+    'customer_name': 'شركة العــــلاني',
+    'custom_party_balance': '24 شارع النصر تفرع مصطفي صدقي -مدينة الهدى -القاهرة',
     'invoice_date': '1 يناير 2020',
     'due_date': '10 يناير 2020',
-    'invoice_no': '494512',
+    'name4512',
     'items': [
-        {'description': 'تصميم نشرة', 'unit_price': '$10', 'qty': 1, 'total': '$10'},
-        {'description': 'تصميم كتيب', 'unit_price': '$5', 'qty': 2, 'total': '$10'},
-        {'description': 'بطاقة أعمال', 'unit_price': '$15', 'qty': 3, 'total': '$45'},
-        {'description': 'تصميم لوريم إيبسوم', 'unit_price': '$25', 'qty': 4, 'total': '$100.00'},
-        {'description': 'تصميم لوريم إيبسوم', 'unit_price': '$10', 'qty': 5, 'total': '$50.00'},
+        {'item_code': 'تصميم نشرة', 'rate': '$10', 'qty': 1, 'base_total': '$10'},
+        {'item_code': 'تصميم كتيب', 'rate': '$5', 'qty': 2, 'base_total': '$10'},
+        {'item_code': 'بطاقة أعمال', 'rate': '$15', 'qty': 3, 'base_total': '$45'},
+        {'item_code': 'تصميم لوريم إيبسوم', 'rate': '$25', 'qty': 4, 'base_total': '$100.00'},
+        {'item_code': 'تصميم لوريم إيبسوم', 'rate': '$10', 'qty': 5, 'base_total': '$50.00'},
     ],
-    'sub_total': '$535',
+    'base_total': '$535',
     'tax': '$0.00',
-    'grand_total': '$535',
+    'base_total': '$535',
     'account_no': '123456789',
     'account_name': 'فولاني ابن علاني',
     'bank_details': 'أضف تفاصيل بنكية',
     'phone': '1234-567-890',
     'website': 'yourbusinesswebsite.com',  # لا توجد ترجمة محددة لاسم الموقع في هذا السياق
     'email': 'your@email.com',
-    'contact_address': 'عمر مدينة نصر  - 40',
+    'contact_custom_party_balance': 'عمر مدينة نصر  - 40',
     'terms': 'تطبق الشروط والأحكام المتفق عليها طبقا للقواعد ',
 }
 
